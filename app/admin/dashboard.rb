05976cacba5a5d031b227756b5f599289565841b
mankumar07 <mankumar@bestpeers.com>
2023-06-30 16:15:12 +0530

    commit for dastbord changes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,23 +12,23 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Stocks" do
+          ul do
+            Stock.all.map do |stock|
+              li link_to(stock.symbol)
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
+      end
+    end
     panel "All Portfolios" do
       ul do
         Portfolio.all.map do |portfolio|
